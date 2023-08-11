ec91bf7e95bb2e42c5409c008e92decddfa0b1ba
민성현/연구원/IT SW플랫폼개발Project(seonghyeon.min@lge.com) <seonghyeon.min@lge.com>
2023-08-11 17:45:11 +0900

    Delete Mode & git clone etc..

--- README.md
+++ README.md
@@ -4,3 +4,8 @@ to proceed automatically doing job.
 # 230810
  - write draft version (webos_consumer_script.py)
  - by reference webos_script.py
+
+# 230811
+ - modify some function
+ - being available for Delete function
+ - (TBD) Add function 
--- webos_consumer_script.py
+++ webos_consumer_script.py
@@ -4,6 +4,9 @@ import platform
 import shutil
 import sys
 import csv
+import json
+
+MODULE_NAME = 'starfish-customization-consumer'
 
 PopularSocNameForVersion23 = {
     1: 'kf23f',
@@ -15,12 +18,11 @@ PopularBranchNames = {
     'kid23q' : [
                 '@52.webos4tv', # for kid23q from wall
                ],
-    'kif23f' : [
+    'kf23f' : [
                 '@it.52.webos4tv.49',
                 '@it.52.webos4tv.56',
                ] # for kf23f from itsw
 }
-
 '''
 Make build-starfish git dir temporarily
 The temp directory name will be changed
@@ -28,26 +30,43 @@ return: temp git directory
 '''
 
 def CloneConsumerGit() :
-    SoC, branch = GetSoCBranchName()
+    SoC, branchname = GetSoCBranchName()
     # make tmp dir
-    progdir = os.path.dirname(os.path.realpath(__file__))
-    tmpdir_wall = os.path.join(progdir, 'tmp_wall_starfish-customization-consumer')
-    tmpdir_itsw = os.path.join(progdir, 'tmp_itwsw_starfish-customization-consumer')
+    progdir = os.getcwd() # get current directory
+    
+    if SoC == 'kf23f' :
+        cmdline = 'mkdir cached_itsw_starfish_customization_consumer'
+    elif SoC == 'kid23q' :
+        cmdline = 'mkdir cached_wall_starfish_customization_consumer'
+    else :
+        print('Unknown SoC ' + SoC)
+        exit()    
+    
+    tmpdir_wall = os.path.join(progdir, 'cached_wall_starfish_customization_consumer')
+    tmpdir_itsw = os.path.join(progdir, 'cached_itsw_starfish_customization_consumer')
 
+        
     # clone git
     if SoC == 'kf23f' :
-        cmdline = 'git clone "ssh://seonghyeon.min@itsw.lge.com:29418/starfish/starfish-customization-consumer" && + \
-                   scp -p -P 29418 seonghyeon.min@itsw.lge.com:hooks/commit-msg "starfish-customization-consumer/.git/hooks/" ' + tmpdir_itsw
+        if not os.path.exists(tmpdir_itsw) :
+            os.system(cmdline)
+        os.chdir(tmpdir_itsw)
+        cmdline = 'git clone "ssh://seonghyeon.min@itsw.lge.com:29418/starfish/starfish-customization-consumer" && scp -p -P 29418 seonghyeon.min@itsw.lge.com:hooks/commit-msg "starfish-customization-consumer/.git/hooks/"'
         gitdir = os.path.join(progdir, tmpdir_itsw)
     elif SoC == 'kid23q' :
-        cmdline = 'git clone "ssh://seonghyeon.min@wall.lge.com:29448/starfish/starfish-customization-consumer" && +\
-                  (cd "starfish-customization-consumer" && gitdir=$(git rev-parse --git-dir); + \
-                   curl -o ${gitdir}/hooks/commit-msg https://wall.lge.com/static/commit-msg ; chmod +x ${gitdir}/hooks/commit-msg) ' + tmpdir_wall
+        if not os.path.exists(tmpdir_wall) :
+            os.system(cmdline)
+        os.chdir(tmpdir_wall)
+        cmdline = 'git clone "ssh://seonghyeon.min@wall.lge.com:29448/starfish/starfish-customization-consumer" && (cd "starfish-customization-consumer" && gitdir=$(git rev-parse --git-dir); curl -o ${gitdir}/hooks/commit-msg https://wall.lge.com/static/commit-msg ; chmod +x ${gitdir}/hooks/commit-msg)'
         gitdir = os.path.join(progdir, tmpdir_wall)
     else :
         print('Unknown SoC ' + SoC )
         exit()
 
+    os.system(cmdline) # git-clone cmdline  
+    gitdir = os.path.join(gitdir, MODULE_NAME)
+    os.chdir(gitdir)
+    
     if not os.path.exists(gitdir) :
         os.system(cmdline)
 
@@ -55,8 +74,11 @@ def CloneConsumerGit() :
         print('  ** Check your git permission')
         print('  ** Check cmdline: "{0}"'.format(cmdline))
         return ''
+    
+    cmdline = 'git checkout ' + branchname
+    os.system(cmdline)
 
-    return gitdir
+    return gitdir, SoC, branchname
 
 
 '''
@@ -69,7 +91,7 @@ def GetSoCBranchName() :
     SoC, branchname = None, None
     PopularSoC, PopularBranchname = PopularSocNameForVersion23, PopularBranchNames
     while True :
-        print('-- Select SoC--'.center(40))
+        print('-- Select SoC --'.center(40))
         for key in PopularSoC.keys() :
             print('{0:10}. {1}'.format(key, PopularSoC[key]))
         print('---------------------------------'.center(40))
@@ -98,7 +120,7 @@ def GetSoCBranchName() :
                 break
             else :
                 try :
-                    flag = str(input('> Warning: cannot finr "' + inputstr + '". Do you want to see SoC list (y/N)'))
+                    flag = str(input('> Warning: cannot find "' + inputstr + '". Do you want to see SoC list (y/N)'))
                 except SyntaxError :
                     flag = ''
 
@@ -123,7 +145,7 @@ def GetSoCBranchName() :
 
         if inputstr.isdigit() :
             inputnum = int(inputstr)
-            if inputnum in range(len(PopularBranchname[SoC])-1) :
+            if inputnum in range(len(PopularBranchname[SoC])) :
                 branchname = PopularBranchname[SoC][inputnum]
                 break
             else :
@@ -137,20 +159,62 @@ def GetSoCBranchName() :
             else :
                 print('> Warning: input right branch-name')
                 continue
-
-    cmdline = 'git checkout ' + branchname
-    os.system(cmdline)
-
+            
     return SoC, branchname
 
 '''
 Get app list to add or delete
 return App list, mode(add or delete)
 '''
-def GetAppList() :
-    # 뭔가 삭제할 앱을 가져와야할 듯? 국가마다 앱리스트가 다름...
-    gitdir = CloneConsumerGit()
-    Countrys = os.listdir(os.path.join(gitdir, 'SoC', 'launchpoints'))
+def GetAppList(currentdir) :
+    countrys = os.listdir(currentdir)
 
-    for country in Countrys :
+    Apps = input('> Input App names you want to modify : ').replace(' ', '').split(',')
+    print('Apps list = ', Apps)
+    
+    if Apps != [] :
+        for country in countrys :
+            AppDir = os.path.join(currentdir, country, 'applist.json')
+            with open(AppDir, 'rb') as file :
+                content = file.read()
+                
+            content_decoded = content.decode('utf-8')
+            data = json.loads(content_decoded)
+            
+            for key, value in data.items() :
+                for App in Apps :
+                    if App in value :
+                        data[key].remove(App)
+            
+            json_str = json.dumps(data, indent=4).replace('\n', '\r\n')
+            json_bytes = json_str.encode('utf-8')
+            
+            with open(AppDir, 'wb') as file :
+                file.write(json_bytes)
+    
+    elif Apps == [] :
+        print('> There is no app to modify')
 
+            
+def DoCommit(branchname) :
+    cmdline = ['git status', 'git add .', 'git commit']
+    for cmd in cmdline :
+        os.system(cmd)
+        
+    # after writing commit message 
+    os.system('git push origin HEAD:refs/for/' + branchname)
+
+def CliMode() :
+    curdir = os.getcwd()
+    progdir = os.path.dirname(os.path.realpath(__file__))
+    print('<< Start starfish-customization-consumer Modification for CLI mode >>')
+    tempgitdir, SoC, branchname = CloneConsumerGit()
+    os.chdir(os.path.join(tempgitdir, SoC, 'launchpoints'))
+    GetAppList(os.getcwd())
+    DoCommit(branchname)
+
+if __name__ == '__main__' :
+    print('<< Start Modification of Apps >>')
+    print('')
+    CliMode()
+    print('<< End Modification of Apps >>')
